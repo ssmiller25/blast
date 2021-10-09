3956897d2b6e9ba8cb8fc01fb498979e8b20ce21
Steve Miller <me@r15cookie.com>
2021-10-09 09:33:24 -0400

    Updates for VR Desktop

---
+++ scripts/wksetup/windows/setup-vrworkstation.ps1
@@ -0,0 +1,7 @@
+REM Specific commands for the VR workstation (HTC Vive specifically)
+
+choco install steam
+echo "Please start steam manually to pull latest version, and log in"
+pause
+echo "Open up https://www.vive.com/us/setup/pc-vr/ and setup HTC Vive Headset"
+pause
